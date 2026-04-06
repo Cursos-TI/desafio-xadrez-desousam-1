6c16dce1dbd8af9a81702753f3d45a9dd5115db9
de Sousa <mariliacs0239@gmail.com>
2026-04-06 15:29:34 +0000

    bisco com recursividade e loop aninhado

--- xadrez.c
+++ xadrez.c
@@ -14,5 +14,20 @@ void moverRainha(int casas){
     moverRainha(casas - 1);
 }
 
+// BISPO (RECURSIVIDADE + LOOP ANINHADO)
+void moverBispo(int casas){
+    if(casas == 0) return;
+
+    //loop externo (vertical)
+    for(int i = 0; i < 1; i++){
+        //loop interno (horizontal)
+        for(int j = 0; j < 1; j++){
+            printf("cima, direita\n");
+        }
+    }
+
+    moverBispo(casas - 1);
+}
+
     return 0;
 }
